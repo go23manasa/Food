6c905989f9f37f30731313d62bbea97999ac6e57
Manasa <manasa.g233@gmail.com>
2025-06-06 13:17:33 +0530

    added vercel.json and updated AppStore

--- src/components/AppStore/AppStore.jsx
+++ src/components/AppStore/AppStore.jsx
@@ -8,7 +8,7 @@ const AppStore = () => {
   return (
     <>
         <div className="bg-gray-100 dark:bg-gray-800 py-14">
-            <div className="container -mt-30">
+            <div className="container">
                 <div className="grid grid-cols-1 sm:grid-cols-2 items-center gap-4">
                     <div className="space-y-6 max-w-xl mx-auto">
                         <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold dark:text-gray-400 text-gray-700">
